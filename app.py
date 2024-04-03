cbdd724289130fd1f647db26a8f34a7c4ad0b73d
awouak <144552908+awouak@users.noreply.github.com>
2024-04-03 11:22:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def home():
     return render_template('index.html')
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
